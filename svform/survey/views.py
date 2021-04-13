975236a0f9a82ba0897a7861fda91374a4aed1ce
sujin <sjhur0417@gmail.com>
2021-04-13 16:17:54 +0900

    migration + make superuser

--- svform/survey/views.py
+++ svform/survey/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def index(request):
+    return HttpResponse('hello')
 
 def introduce(request):
     return HttpResponse('introduceapp')
