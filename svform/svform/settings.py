1273a5c98f7cdca2c5ae1666d8637131b44b776b
sujin <sjhur0417@gmail.com>
2021-07-08 16:47:19 +0900

    add alloewed hosts

--- svform/svform/settings.py
+++ svform/svform/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.209.10.13']
 
 
 # Application definition
