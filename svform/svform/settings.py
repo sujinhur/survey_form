23baa7dffc6685f72417c13b2d202ed47d0d2b98
sujin <sjhur0417@gmail.com>
2021-11-06 16:27:32 +0900

    host name

--- svform/svform/settings.py
+++ svform/svform/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.36.72.53']
 
 
 # Application definition
